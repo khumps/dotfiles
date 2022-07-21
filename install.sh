5891305a83e779d4ffe923024a8fa786800958d2
Kevin Humphreys <kmanh999@gmail.com>
2022-07-20 21:18:48 -0400

    oinstall Hasklug font

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ set -e
 
 GITHUB_USERNAME="khumps"
 
+echo "[INSTALL] Fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hasklig.zip
+mkdir -p ~/.local/share/fonts
+unzip Hasklig.zip -d ~/.local/share/fonts/
+rm Hasklig.zip
+
 echo "[INSTALL] Chezmoi"
 export BINDIR=$HOME/.local/bin
 sh -c "$(curl -fsLS chezmoi.io/get)"
