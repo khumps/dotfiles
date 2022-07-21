d1f8866b1819985c96c6da9490a839a385d65770
Kevin Humphreys <kmanh999@gmail.com>
2022-07-21 04:22:39 +0000

    move starship, font, and zsh suggestion installs to run_once scripts

--- install.sh
+++ install.sh
@@ -3,14 +3,7 @@ set -e
 
 GITHUB_USERNAME="khumps"
 
-echo "[INSTALL] Fonts"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hasklig.zip
-mkdir -p ~/.local/share/fonts
-unzip Hasklig.zip -d ~/.local/share/fonts/
-rm Hasklig.zip
 
-echo "[INSTALL] ZSH auto-suggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 
 echo "[INSTALL] Chezmoi"
